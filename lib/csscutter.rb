00a75d708874aa37eed56b10c3e42ed7e7a8e53d
m16a1 <a741su@gmail.com>
2012-11-10 16:08:27 +0400

    feature: remove unnecessary whitespace

--- lib/csscutter.rb
+++ lib/csscutter.rb
@@ -5,11 +5,19 @@ class CssCutter
 
   def initialize(input)
     @output = input
+    remove_whitespace
     remove_comments
     @output
   end
 
   private
+  def remove_whitespace
+    @output.gsub! /\s+/, ' '
+    @output.gsub! /\s*\{\s*/, '{'
+    @output.gsub! /\s*\}\s*/, '}'
+    @output.gsub! /(\S):\s(\S)/, '\1:\2'
+  end
+
   def remove_comments
     @output.gsub! /\/\*.*?\*\//, ''
   end
--- spec/lib/csscutter_spec.rb
+++ spec/lib/csscutter_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe CssCutter do
   specify { CssCutter.should have_constant(:VERSION) }
+  it 'removes unnecessary white space' do
+    cutter = CssCutter.new fixture('whitespace.css')
+    cutter.output.should == fixture('whitespace.min.css')
+  end  
   it 'removes comments' do
     cutter = CssCutter.new fixture('comment.css')
     cutter.output.should == fixture('comment.min.css')
