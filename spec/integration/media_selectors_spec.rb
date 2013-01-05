a150c71c14091cd1b9ff97479d85934523356baf
m16a1 <a741su@gmail.com>
2013-01-05 17:19:34 +0400

    remove whitespace around comma

--- spec/integration/media_selectors_spec.rb
+++ spec/integration/media_selectors_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'media selectors', integration: true do
   let(:cutter) {CssCutter.new }
-  it 'optimized fine' do
+  it 'single selector' do
     code = <<-CSS
       @media all and (min-width: 640px) {
         #media-queries-1 {
@@ -10,6 +10,18 @@ describe 'media selectors', integration: true do
         }
       }
     CSS
-    cutter.optimize(code).should == '@media all and (min-width:640px){#media-queries-1{background-color:#0f0}}'
+    cutter.optimize(code).should == '@media all and (min-width:640px){#media-q'\
+      'ueries-1{background-color:#0f0}}'
+  end
+  it 'many selectors' do
+    code = <<-CSS
+      @media only all and (max-width: 50em), only all and (max-device-width: 800px) {
+        div {
+          display: block;
+        }
+      }
+    CSS
+    cutter.optimize(code).should == '@media only all and (max-width:50em),only'\
+     ' all and (max-device-width:800px){div{display:block}}'
   end
 end
