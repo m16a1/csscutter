19aefccb5665e39d321f1e37808038b89d92747b
m16a1 <a741su@gmail.com>
2013-01-17 11:33:19 +0400

    performance tuning

--- lib/csscutter/cleaner.rb
+++ lib/csscutter/cleaner.rb
@@ -1,21 +1,12 @@
 class CssCutter::Cleaner < String
   def remove_whitespace
     strip
-      .gsub(/\s+/, ' ')
-      .gsub(/\s*\{\s*/, '{')
-      .gsub(/\s*\}\s*/, '}')
-      .gsub(/(\S+)\s*:\s*([^\{\};]+?)(?=[\};])/, '\1:\2')
-      .gsub(/(\([^\)]*)(\S)\s*:\s*/, '\1\2:')
-      .gsub(/\s*([,;=\+\>~])\s*/, '\1')
-      .gsub('( ', '(')
-      .gsub(' )', ')')
-      .gsub('[ ', '[')
-      .gsub(' ]', ']')
-      .gsub(' ~=', '~=')
-      .gsub(' $=', '$=')
-      .gsub(' *=', '*=')
-      .gsub(' |=', '|=')
-      .gsub(' ^=', '^=')
+      .remove_repeated
+      .remove_around_curly_brackets
+      .remove_between_property_and_value
+      .remove_in_selectors
+      .remove_around_other_brackets
+      .remove_in_attribute_selectors
   end
 
   def remove_trailing_semicolons
@@ -29,5 +20,33 @@ class CssCutter::Cleaner < String
 
   def remove_comments
     gsub /\/\*.*?\*\//, ''
+  end
+
+  def remove_repeated
+    gsub(/\s+/, ' ')
+  end
+
+  def remove_around_curly_brackets
+    gsub(/\s*\{\s*/, '{')
+      .gsub(/\s*\}\s*/, '}')
+  end
+
+  def remove_between_property_and_value
+    gsub(/(?:^|\})[^\{:]+\s+:+[^\{]*\{/) { |m| m.gsub ':', '_COLON_' }
+      .gsub(/\s?:\s?/, ':')
+      .gsub('_COLON_', ':')
+  end
+
+  def remove_around_other_brackets
+    gsub(/([\(\[]) /, '\1')
+      .gsub(/ ([\)\]])/, '\1')
+  end
+
+  def remove_in_selectors
+    gsub(/\s*([,;=\+\>~])\s*/, '\1')
+  end
+
+  def remove_in_attribute_selectors
+    gsub(/ ([\~\$\*\|\^])=/, '\1=')
   end
 end
